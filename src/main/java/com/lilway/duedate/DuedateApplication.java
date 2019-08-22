0f228ed199ae6a6259ca6fbefbc99dcdf6815f31
Lilla <vajdalil.hu@gmail.com>
2019-08-22 11:34:26 +0200

    init

---
+++ src/main/java/com/lilway/duedate/DuedateApplication.java
@@ -0,0 +1,13 @@
+package com.lilway.duedate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuedateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuedateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lilway/duedate/DuedateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lilway.duedate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DuedateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
